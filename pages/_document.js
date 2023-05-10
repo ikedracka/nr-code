bfd19ad055c77cb7365f97e6d2c270245e0e4d51
Ilona Kedracka <ilona.kedracka@wpengine.com>
2023-05-10 15:43:14 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { getAnrScript } from 'utils/anr'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {getAnrScript()}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
