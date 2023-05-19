6840160537079215e9355eb8894f4e4acbb74ee6
Ilona Kedracka <ilona.kedracka@wpengine.com>
2023-05-19 18:54:26 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { getAnrScript } from 'utils/anr'
+import { getAnrScript } from "../utils/anr"
 
 class MyDocument extends Document {
   render() {
